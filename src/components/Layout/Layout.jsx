22abb25e081c1502441343d7aaa414f7ed4497c7
ayamahmoud76 <ayaamahmoud155@gmail.com>
2026-06-04 11:35:26 +0300

    fix Footer import case

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../navbar/Navbar'
-import Footer from '../footer/Footer'
+import Footer from '../Footer/Footer'
 
 export default function Layout() {
  const [isOpen , setIsOpen]= useState(false)
